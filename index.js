6c838d97dc47cf652c8fd7387c92e4e5cd6ee6d2
Salman Waheed <mkdirenv@gmail.com>
2021-02-24 09:15:05 +0400

    add private methods and properties

--- index.js
+++ index.js
@@ -4,32 +4,36 @@
  */
 
 class DeletifySearch {
+  // hash (#) to make properties or methods private
+  #searchInputId
+  #resultsContainerId
+
   constructor({
     searchInputId=null,
     resultsContainerId=null,
     noResultsText='No Results found'
   }) {
-    this.searchInputId = searchInputId
-    this.resultsContainerId = resultsContainerId
+    this.#searchInputId = searchInputId
+    this.#resultsContainerId = resultsContainerId
     this.noResultsText = noResultsText
     this.search()
   }
 
-  getSearchInputId() {
-    return document.getElementById(this.searchInputId)
+  #getSearchInputId() {
+    return document.getElementById(this.#searchInputId)
   }
 
-  getResultsContainerId() {
-    return document.getElementById(this.resultsContainerId)
+  #getResultsContainerId() {
+    return document.getElementById(this.#resultsContainerId)
   }
 
-  getElements() {
-    return this.getResultsContainerId().getElementsByTagName('li')
+  #getElements() {
+    return this.#getResultsContainerId().getElementsByTagName('li')
   }
 
   search() {
-    this.getSearchInputId().addEventListener('keyup', (e) => {
-      for (let list of this.getElements()) {
+    this.#getSearchInputId().addEventListener('keyup', (e) => {
+      for (let list of this.#getElements()) {
         let targetValue = e.target.value.toLowerCase()
         let matchValue = list.getElementsByTagName("a")[0].textContent.toLowerCase()
         // let matchValue = list.getElementsByTagName("a")[0].getAttribute('title').toLowerCase()
